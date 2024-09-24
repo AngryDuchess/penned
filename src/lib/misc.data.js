de8f6c9cdd6cec3fe24a61b6d96bd2e7681b42d0
Mahama Hamida <mahamahamida@gmail.com>
2024-09-24 20:35:44 +0000

    Add initial blog data to the project

---
+++ src/lib/misc.data.js
@@ -0,0 +1,25 @@
+const blogs = [
+    {
+        id: "1",
+        title: "Teddy my big dog",
+        date_created: "August 2020",
+        image: "https://res.cloudinary.com/dn5ks1ljf/image/upload/v1715422433/cld-sample.jpg",
+        details: "Teddy is a big dog in a big world. He is a very good dog but can be bad as well."
+    },
+    {
+        id: "2",
+        title: "Healthy diet, Happy life",
+        date_created: "August 2020",
+        image: "https://res.cloudinary.com/dn5ks1ljf/image/upload/v1715422434/cld-sample-4.jpg",
+        details: "Teddy is a big dog in a big world. He is a very good dog but can be bad as well."
+    },
+    {
+        id: "3",
+        title: "Furry friends",
+        date_created: "August 2020",
+        image: "https://res.cloudinary.com/dn5ks1ljf/image/upload/v1723154814/Portfolio%20assets/thumbnail-furry_qp4zbt.png",
+        details: "Teddy is a big dog in a big world. He is a very good dog but can be bad as well."
+    }
+]
+
+export {blogs}

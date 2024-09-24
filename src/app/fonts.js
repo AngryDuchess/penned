a98255177609438d13913b0aa9b8415f333b914c
Mahama Hamida <mahamahamida@gmail.com>
2024-09-24 20:36:07 +0000

    Add font configuration for Inter and Gowun_Batang

---
+++ src/app/fonts.js
@@ -0,0 +1,12 @@
+import { Inter, Gowun_Batang } from "next/font/google"
+
+export const inter = Inter({
+    subsets: ["latin"],
+    display: 'swap'
+})
+
+export const gowun_batang = Gowun_Batang({
+    subsets: ["latin"],
+    display: 'swap',
+    weight: '700'
+})

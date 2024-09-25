8249a95bd77c8cc3f0169f765e4f61bb7a95113b
Mahama Hamida <mahamahamida@gmail.com>
2024-09-25 15:35:26 +0000

    Refactor blog page component to display selected blog details

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,6 +1,25 @@
+"use client";
+import { useBlog } from "@/context/blog-context";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function BlogDetails() {
-return (
-    <p className="h-screen w-screen flex items-center justify-center">Hiii there this is a blog post</p>
-)
+  const { selectedBlog } = useBlog();
+  const router = useRouter();
+
+  return (
+    <div className="px-4 md:px-40 py-14">
+      <h1 className="text-2xl mb-4">{selectedBlog.title}</h1>
+      {selectedBlog.blogImage && (
+        <Image
+          src={selectedBlog.blogImage}
+          alt="Blog Thumbnail"
+          width={400}
+          height={300}
+          className="mb-4"
+        />
+      )}
+      <p>{selectedBlog.blogdetails}</p>
+    </div>
+  );
 }

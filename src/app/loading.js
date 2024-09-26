9fd9e419f6f6bd1a5f4df788adc2ac9d22161d27
Mahama Hamida <mahamahamida@gmail.com>
2024-09-26 00:19:09 +0000

    Add Loading component for displaying a loading message

---
+++ src/app/loading.js
@@ -0,0 +1,8 @@
+"use client"
+export default function Loading() {
+    return (
+        <p className="flex flex-col h-screen w-screen justify-center items-center">
+        Your page is loading...And if it doesn&apos;t, your network really sucks!
+        </p>
+    )
+}

0806e2f1f8ec5b7f076fe0eb7af0a848b4da4acf
Mahama Hamida <mahamahamida@gmail.com>
2024-09-26 00:19:25 +0000

    Refactor BlogDetails page layout and add back button
    - Refactor the layout of the BlogDetails page to improve readability and alignment.
    - Added a back button to allow users to navigate back to the homepage.

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,25 +1,57 @@
 "use client";
 import { useBlog } from "@/context/blog-context";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Twitter,Linkedin} from "@/assets/icons";
+import { getDateCreated } from "@/lib/utils";
+import Link from "next/link";
+import { ArrowLeft } from "iconsax-react";
 
 export default function BlogDetails() {
   const { selectedBlog } = useBlog();
   const router = useRouter();
+  useEffect(() => {
+    if (!selectedBlog) {
+      router.push("/");
+    }
+  }, [selectedBlog, router]);
 
+  if (!selectedBlog) {
+    return null;
+  }
   return (
-    <div className="px-4 md:px-40 py-14">
-      <h1 className="text-2xl mb-4">{selectedBlog.title}</h1>
+    <>
+    <Link
+            href="/"
+            className="mx-4 md:mx-40  flex gap-3 py-2 px-3 rounded-lg font-semibold text-sm w-24 text-gray-400 hover:text-pink-500"
+          >
+            <ArrowLeft size={20} />
+            Back
+          </Link>
+    <div className="px-4 md:px-40 py-8 flex flex-col justify-center items-center  gap-8">
+        <div className="flex flex-col gap-3 items-center">
+
+      <p>{getDateCreated()}</p>
+      <h1 className="text-4xl text-center mb-4">{selectedBlog.title}</h1>
+      <div className="flex gap-3">
+        <Link href="https://www.linkedin.com/in/hamidamahama/">
+        <Linkedin />
+        </Link>
+        <Link href="https://x.com/hamida_mahama">
+        <Twitter />
+        </Link>
+        </div>
+
+      </div>
       {selectedBlog.blogImage && (
-        <Image
-          src={selectedBlog.blogImage}
-          alt="Blog Thumbnail"
-          width={400}
-          height={300}
-          className="mb-4"
-        />
+          <div className="flex flex-col gap-6 justify-center items-start max-w-3xl w-full">
+
+        <div className="w-full h-96 bg-center rounded-2xl" style={{backgroundImage:`url(${selectedBlog.blogImage})`}}>
+          </div>
+          <p className="text-left items-start">{selectedBlog.blogdetails}</p>
+        </div>
       )}
-      <p>{selectedBlog.blogdetails}</p>
     </div>
+      </>
   );
 }

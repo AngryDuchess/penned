dc03f1d17b5cc35f2c647e776581b7188ac1752c
Mahama Hamida <mahamahamida@gmail.com>
2024-10-31 18:43:45 +0000

    Refactor metadata in layout.js to update app description and add open graph and Twitter card information

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,13 @@ export const metadata = {
       width: "1200",
       height: "630"
     }
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@hamida_mahama",
+    title: "Penned",
+    description:"A journaling app experiment by Hamida Mahama",
+    image: "/images/twitter-image.png"
   }
 
 };
@@ -25,12 +32,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* OPEN GRAPH META TAGS */}
         <meta property="og:title" content="Penned" />
         <meta property="og:description" content="A journaling app experiment by Hamida Mahama" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/images/open-graph-img.png" />
+        <meta property="og:image:type" content="" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+
+        {/* TWITTER CARD */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@hamida_mahama" />
+        <meta name="twitter:title" content="Penned" />
+        <meta name="twitter:description" content="A journaling app experiment by Hamida Mahama" />
+        <meta name="twitter:image" content="/images/open-graph-img.png" />
       </head>
       <body className={`${inter.className}`}>
       <Link href="/" className="flex items-center justify-center py-9">

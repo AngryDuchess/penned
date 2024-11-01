0b6043890ffc14f6a4dfc61ad0a0f9cb4c87f81b
Mahama Hamida <mahamahamida@gmail.com>
2024-11-01 11:14:28 +0000

    Refactor metadata image URLs in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
     site: "@hamida_mahama",
     title: "Penned",
     description:"A journaling app experiment by Hamida Mahama",
-    image: "/images/twitter-image.png"
+    image: "https://res.cloudinary.com/dn5ks1ljf/image/upload/v1730459513/Portfolio%20assets/twitter-image_mvqvw2.png"
   }
 
 };
@@ -50,7 +50,7 @@ export default function RootLayout({ children }) {
         <meta name="twitter:site" content="@hamida_mahama" />
         <meta name="twitter:title" content="Penned" />
         <meta name="twitter:description" content="A journaling app experiment by Hamida Mahama" />
-        <meta name="twitter:image" content="/images/open-graph-img.png" />
+        <meta name="twitter:image" content="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1730459513/Portfolio%20assets/twitter-image_mvqvw2.png" />
       </head>
       <body className={`${inter.className}`}>
       <Link href="/" className="flex items-center justify-center py-9">

f750ab29c547cf49cc5a0aa115a1ef4a9b0abbf7
Mahama Hamida <mahamahamida@gmail.com>
2024-11-01 14:52:31 +0000

    Refactor metadata image URLs in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,20 +13,26 @@ export const metadata = {
     type: "website",
     image:
     {
-      url: "/images/open-graph-img.png",
+      url: "/images/open-graph-image.png",
       width: "1200",
       height: "630"
     }
   },
   twitter: {
-    card: "summary",
     card: "summary_large_image",
-    site: "@hamida_mahama",
     title: "Penned",
-    description:"A journaling app experiment by Hamida Mahama",
-    image: "https://res.cloudinary.com/dn5ks1ljf/image/upload/v1730459513/Portfolio%20assets/twitter-image_mvqvw2.png"
-  }
-
+    description: "A journaling app experiment by Hamida Mahama",
+    creator: "@hamida_mahama",
+    images: [
+      {
+        url: "you_url_here",
+        blurDataURL: "/images/twitter-image.png",
+        width: 1200,
+        height: 630,
+        alt: "twitter card image",
+      },
+    ],
+  },
 };
 
 
@@ -39,7 +45,7 @@ export default function RootLayout({ children }) {
         <meta property="og:title" content="Penned" />
         <meta property="og:description" content="A journaling app experiment by Hamida Mahama" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/images/open-graph-img.png" />
+        <meta property="og:image" content="/images/open-graph-image.png" />
         <meta property="og:image:type" content="" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
@@ -50,7 +56,7 @@ export default function RootLayout({ children }) {
         <meta name="twitter:site" content="@hamida_mahama" />
         <meta name="twitter:title" content="Penned" />
         <meta name="twitter:description" content="A journaling app experiment by Hamida Mahama" />
-        <meta name="twitter:image" content="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1730459513/Portfolio%20assets/twitter-image_mvqvw2.png" />
+        <meta name="twitter:image" content="/images/twitter-image.png" />
       </head>
       <body className={`${inter.className}`}>
       <Link href="/" className="flex items-center justify-center py-9">
